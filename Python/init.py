81c8041391e4c2f4d10eea9950c716bf45547108
Florin Burdea <florin.burdea@gmail.com>
2017-09-13 21:52:34 +0200

    Multiple URLs

--- Python/init.py
+++ Python/init.py
@@ -1,3 +1,19 @@
-//Init Python file
+#Init Python file
+#Located in the core source
 
-__ini__()
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/<user>')
+def index(user=None):
+	return render_template('user.html', user = user)
+
+@app.route('/shoping')
+def shoping():
+    food = ["Cheese","Tuna","Salad" ]
+    return render_template("shoping.html", food = food )
+
+if __name__=="__main__":
+	app.run()
+
